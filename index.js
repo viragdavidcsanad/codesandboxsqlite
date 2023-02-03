8fc8ae918d320f12d2d291fd6a25cab248ec20b6
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-02-03 17:24:02 +0100

    index.html, index.js, style.css

---
+++ index.js
@@ -0,0 +1,25 @@
+const link = "https://first-azure-project.azurewebsites.net/api/features";
+const $jsContent = document.querySelector(".js_content");
+
+// 
+// http://localhost:8901/api/features
+
+const fresh = fetch(link)
+  .then((data) => data.json())
+  .then((features) => {
+    $jsContent.innerHTML = ``;
+    features.map(
+      ({id, feature, version, year}) =>
+        ($jsContent.innerHTML += `
+        <li class="feature">${id}
+          <ul>
+            <li>${feature}</li>
+            <li>${version}</li>
+            <li>${year}</li>
+          </ul>
+        </li>`)
+    );
+  });
+
+fresh;
+
