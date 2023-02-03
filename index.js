13bfcb2292ac32668b48bb151ba948cf8f942bc7
viragdavidcsanad <viragdavidcsanad@gmail.com>
2023-02-03 19:56:07 +0100

    index.js fetch modifying

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const $jsContent = document.querySelector(".js_content");
 //
 // http://localhost:8901/api/features
 
-const headers = new Headers();
-headers.append("Accept", "application/json");
-const request = new Request(link, {
-  method: "POST",
-  headers,
-  mode: "cors",
-});
+// const headers = new Headers();
+// headers.append("Accept", "application/json");
+// const request = new Request(link, {
+//   method: "POST",
+//   headers,
+//   mode: "cors",
+// });
 
-fetch(request)
+fetch(link)
   .then((data) => data.json())
   .then((features) => {
     $jsContent.innerHTML = ``;
